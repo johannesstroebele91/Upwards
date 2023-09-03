b7128d70c2f0b57725c39c23560ca20f314448e6
Johannes Ströbele <johannes.stroebele@partner.bmwgroup.com>
2023-09-03 23:53:59 +0200

    Experiment with new formatting for each item

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,62 @@
+import React, {useState} from "react";
 import Link from 'next/link'
 import dbConnect from '../lib/dbConnect'
 import Pet, { Pets } from '../models/Pet'
 import { GetServerSideProps } from 'next'
+import {Button, Card, Col, Progress, Row, Typography} from "antd";
+import moment from "moment";
+import {LeftOutlined, RightOutlined} from "@ant-design/icons";
+const {Paragraph, Title} = Typography;
 
 type Props = {
   pets: Pets[]
 }
 
+function getSuccessRateForDay(date: moment.Moment, habits: Pets[]): number {
+    /* TODO fix later
+        const habitsDone = habits.filter(habit => {
+        if (!habit.doneHistory) {
+            return false;
+        }
+        return habit.doneHistory.some(doneDate => moment(doneDate).isSame(date, 'day'));
+    });
+    const successRate = (habitsDone.length / habits.length) * 100;
+    return Math.round(successRate);*/
+    return 75;
+}
+
+
 const Index = ({ pets }: Props) => {
+  const [date, setDate] = useState(moment());
   return (
     <>
+        <Title style={{marginTop: 24}}>Hey there, Michelle!</Title>
+        <Row gutter={[16, 24]} align="middle">
+        <Col flex="auto">
+          <Title level={4} style={{marginTop: 10}}>{date.format("ddd, MMM. D")}</Title>
+        </Col>
+        <Col flex="80px">
+          <Button icon={<LeftOutlined/>} onClick={() => setDate(moment(date).subtract(1, 'day'))}
+          ></Button>
+          <Button icon={<RightOutlined/>} onClick={() => setDate(moment(date).add(1, 'day'))}
+                  disabled={moment().isSame(date, 'day')}></Button>
+        </Col>
+      </Row>
+      <Progress percent={75} showInfo={false}/>
+      <Paragraph italic>{getSuccessRateForDay(date, pets)}% of daily goals achieved</Paragraph>
+      <Row gutter={[16, 8]} style={{flexDirection: 'column'}}>
+        {pets.map(habit => (
+            <Card key={habit.id} title={habit.name}>
+                <p>{habit.age}</p>
+                <p>{habit.owner_name}</p>
+                <p>{habit.species}</p>
+            </Card>
+        ))}
+      </Row>
       {pets.map((pet) => (
         <div key={pet._id}>
           <div className="card">
-            <img src={pet.image_url} />
+            <img src={pet.image_url}  alt={pet.name}/>
             <h5 className="pet-name">{pet.name}</h5>
             <div className="main-content">
               <p className="pet-name">{pet.name}</p>
