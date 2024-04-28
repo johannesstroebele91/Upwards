cca3a44dc4d91a40c76695e5f5388fbc59a2aebe
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-28 17:43:50 +0200

    Implement possibility to edit existing goals (name, weekly goal, and active are working). Categories needs to be corrected.

--- components/HabitCard.tsx
+++ components/HabitCard.tsx
@@ -1,10 +1,10 @@
-import {Badge, Card, Col, Row, Select, SelectProps, Switch, Tooltip} from "antd";
-import {Habit} from "../shared/types";
+import {Badge, Button, Card, Col, Input, Row, Select, SelectProps, Switch, Tooltip} from "antd";
+import {DeleteOutlined, MinusOutlined, PlusOutlined} from "@ant-design/icons";
 import {NextRouter, useRouter} from "next/router";
 import {contentType, defaultCategories, homePath} from "../shared/constants";
 import {mutate} from "swr";
-import React from "react";
-import {DeleteOutlined, MinusOutlined, PlusOutlined} from "@ant-design/icons";
+import {Habit} from "../shared/types";
+import React, {useState} from "react";
 
 interface HabitCardProps {
     habit: Habit;
@@ -16,28 +16,26 @@ const convertCategories = (categories: string[]): SelectProps['options'] => {
     );
 };
 
-/* TODO add edit habit using PUT method for editing an existing entry in the mongodb database. */
-const putData = async (form: Habit, router: NextRouter) => {
+const putData = async (changedHabit: Habit, router: NextRouter) => {
     const {id} = router.query;
 
     try {
-        const res = await fetch(`/api/habits/${id}`, {
+        const res = await fetch(`/api/habits/${changedHabit._id}`, {
             method: "PUT",
             headers: {
                 Accept: contentType,
                 "Content-Type": contentType,
             },
-            body: JSON.stringify(form),
+            body: JSON.stringify(changedHabit),
         });
 
-        // Throw error with status code in case Fetch API req failed
         if (!res.ok) {
             console.log(res.status.toString());
         }
 
         const {data} = await res.json();
 
-        await mutate(`/api/habits/${id}`, data, false); // Update the local data without a revalidation
+        await mutate(`/api/habits/${id}`, data, false);
         await router.push(homePath);
     } catch (error) {
         console.log("Failed to update habit");
@@ -46,8 +44,11 @@ const putData = async (form: Habit, router: NextRouter) => {
 
 export const HabitCard: React.FC<HabitCardProps> = ({habit}) => {
     const router = useRouter();
+    const [editing, setEditing] = useState(false);
+    const [name, setName] = useState(habit.name);
+    const [weeklyGoal, setWeeklyGoal] = useState(habit.weeklyGoal);
 
-    async function handleClick(_id: string | undefined): Promise<void> {
+    const handleClick = async (_id: string | undefined) => {
         const contentType = "application/json";
         try {
             const res = await fetch(`/api/habits/${_id}`, {
@@ -58,7 +59,6 @@ export const HabitCard: React.FC<HabitCardProps> = ({habit}) => {
                 },
             });
 
-            // Throw error with status code in case Fetch API req failed
             if (!res.ok) {
                 console.log(res.status.toString());
             }
@@ -66,59 +66,111 @@ export const HabitCard: React.FC<HabitCardProps> = ({habit}) => {
             console.log("Failed to delete habit");
         }
         await router.push(homePath);
-    }
+    };
 
-    let count = 0;
+    const handleEditClick = () => {
+        setEditing(true);
+    };
+
+    const handleActiveChange = (checked: boolean) => {
+        void putData({...habit, active: checked}, router);
+    };
+
+
+    const handleCancelEdit = () => {
+        setEditing(false);
+        setName(habit.name);
+        setWeeklyGoal(habit.weeklyGoal);
+    };
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+    };
+
+    const handleWeeklyGoalChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setWeeklyGoal(parseInt(e.target.value));
+    };
+
+    const handleSave = () => {
+        putData({...habit, name, weeklyGoal}, router);
+        setEditing(false);
+    };
+
     return (
-        <Card actions={[
-            <Tooltip title="Mark Progress (+1)">
-                <PlusOutlined style={{color: '#1677ff'}} className="progress-icon"/>
-            </Tooltip>,
-            <Tooltip title="It happens (-1)">
-                <MinusOutlined style={{color: 'red'}} className="progress-icon"/></Tooltip>
-
-
-        ]}
-              key={habit._id} title={habit.name} style={{margin: 15, width: 350}}>
-            <div style={{position: 'absolute', top: 0, right: 0, margin: '12px 12px 0 0'}}>
-                {/* TODO 0 grey 1-10 red >11-30 orange > 31-... green */}
-                <Tooltip placement="topRight" title="Score for progress of the habit">
-                    <Badge count={count} showZero style={{marginRight: 12}} color={'grey'}/>
+        <Card
+            actions={[
+                <Tooltip title="Mark Progress (+1)">
+                    <PlusOutlined style={{color: '#1677ff'}} className="progress-icon"/>
+                </Tooltip>,
+                <Tooltip title="It happens (-1)">
+                    <MinusOutlined style={{color: 'red'}} className="progress-icon"/>
                 </Tooltip>
-                <Tooltip placement="top" title="Delete the habit">
-                    <DeleteOutlined className="delete-icon" onClick={() => handleClick(habit._id)}
-                                    style={{position: 'relative', top: 2}}/>
-                </Tooltip>
+            ]}
+            key={habit._id}
+            title={habit.name}
+            style={{margin: 15, width: 350}}
+        >
+            <div style={{position: 'absolute', top: 0, right: 0, display: "flex", marginTop: 12}}>
+                {editing && (
+                    <>
+                        <Button type="primary" onClick={handleSave} style={{marginRight: 9}}>Save</Button>
+                        <Button onClick={handleCancelEdit} style={{marginRight: 9}}>Cancel</Button>
+                    </>)}
+                <div style={{margin: '3px 12px 0 0'}}>
+                    <Tooltip placement="topRight" title="Score for progress of the habit">
+                        <Badge count={0} showZero style={{marginRight: 12}} color={'grey'}/>
+                    </Tooltip>
+                    <Tooltip placement="top" title="Delete the habit">
+                        <DeleteOutlined className="delete-icon" onClick={() => handleClick(habit._id)}
+                        />
+                    </Tooltip>
+
+                </div>
             </div>
             <Row style={{display: 'flex', flexDirection: 'column'}} gutter={[12, 12]} wrap={false}>
                 <Col>
                     <Row align="middle">
                         <Col span={4} style={{minWidth: 100}}>Name</Col>
-                        <Col>{habit.name}</Col>
+                        {!editing ? (
+                            <Col>{name} <Button type="link" onClick={handleEditClick}>Edit</Button></Col>
+                        ) : (
+                            <Col>
+                                <Input value={name} onChange={handleNameChange}/>
+                            </Col>
+                        )}
                     </Row>
                 </Col>
                 <Col>
                     <Row align="middle">
                         <Col span={4} style={{minWidth: 100}}>Weekly Goal</Col>
-                        <Col>{habit.weeklyGoal}</Col>
+                        {!editing ? (
+                            <Col>{weeklyGoal} <Button type="link" onClick={handleEditClick}>Edit</Button></Col>
+                        ) : (
+                            <Col>
+                                <Input type="number" value={weeklyGoal.toString()} onChange={handleWeeklyGoalChange}/>
+                            </Col>
+                        )}
                     </Row>
                 </Col>
                 <Col>
                     <Row align="middle">
                         <Col span={4} style={{minWidth: 100}}>Active</Col>
-                        <Col><Switch checked={habit.active} disabled/></Col>
+                        <Col>
+                            <Switch checked={habit.active} onChange={handleActiveChange}/>
+                        </Col>
                     </Row>
                 </Col>
                 <Col>
                     <Row align="middle">
                         <Col span={4} style={{minWidth: 100}}>Categories</Col>
-                        <Col><Select
-                            mode="multiple"
-                            style={{display: 'block', minWidth: '200px'}}
-                            disabled
-                            defaultValue={convertCategories(habit.categories)}
-                            options={defaultCategories}
-                        /></Col>
+                        <Col>
+                            <Select
+                                mode="multiple"
+                                style={{display: 'block', minWidth: '200px'}}
+                                defaultValue={convertCategories(habit.categories)}
+                                options={defaultCategories}
+                            />
+                        </Col>
                     </Row>
                 </Col>
             </Row>
